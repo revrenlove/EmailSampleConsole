d2900fc65827ebdb98c32a5a6b08b4629b47a22d
revrenlove <elrod.dev@gmail.com>
2022-03-27 18:59:24 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+// Email message info
+var toEmailAddress = "Recipient's email address";
+var subjectLine = "Test Subject Line 1002";
+var emailBody = "Test Email Body 1002";
+
+// Your Email Credentials
+var emailAddress = "Your Email Address";
+var emailPassword = "Your password or app key";
+
+var smtpHost = "smtp.gmail.com";
+var smtpPort = 587;
+
+var smtpClient = new SmtpClient
+{
+    Host = smtpHost,
+    Port = smtpPort,
+    EnableSsl = true,
+    DeliveryMethod = SmtpDeliveryMethod.Network,
+    UseDefaultCredentials = false,
+    Credentials = new NetworkCredential(emailAddress, emailPassword)
+};
+
+using var message = new MailMessage(emailAddress, toEmailAddress, subjectLine, emailBody);
+smtpClient.Send(message);
---
+++ README.md
@@ -0,0 +1,9 @@
+# EmailSampleConsole
+
+You'll need to plug in your credentials, and the info for the message.
+
+The variables' names should be straight-forward
+
+*NOTE:* If it barks at you for security shit, I used [this post](https://stackoverflow.com/a/32336/14358945).
+
+Basically, where I have 2FA, I had to generate an app key so this could bypass having to go through the 2FA.
